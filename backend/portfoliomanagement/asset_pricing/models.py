56d01b52ea5e98dbe6e612ad2ae29665aa3c343b
Rushikesh197 <rushikeshgadewar@gmail.com>
2024-03-30 14:54:13 +0530

    Added apps for Assest And Asset Pricing

---
+++ backend/portfoliomanagement/asset_pricing/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from assets.models import Asset
+class asset_pricing(models.Model):
+    ticker = models.ForeignKey(Asset, max_length=10,related_name="asset_price_relation",on_delete=models.CASCADE,default=None)
+    market_value = models.FloatField("Market Value")
+    open=models.FloatField(blank=True,null=True)
+    close=models.FloatField(blank=True,null=True)
+    high=models.FloatField(blank=True,null=True)
+    low=models.FloatField(blank=True,null=True)
+    ft_week_high=models.FloatField(blank=True,null=True)
+    ft_week_low=models.FloatField(blank=True,null=True)
+    month_high=models.FloatField(blank=True,null=True)
+    month_low=models.FloatField(blank=True,null=True)
+    overall_high=models.FloatField(blank=True,null=True)
+    overall_low=models.FloatField(blank=True,null=True)
+
+    ##Day gain loss
+    day_change=models.FloatField(default=0)
+    day_change_percentage=models.FloatField(default=0)
+    
+    timestamp1 = models.DateTimeField(auto_now=False, auto_now_add=False)
+    currency = models.CharField("Enter Currency", max_length=10)
+
+    class Meta:
+        db_table = "asset_pricing1"
+        unique_together = (('ticker', 'timestamp1'))
