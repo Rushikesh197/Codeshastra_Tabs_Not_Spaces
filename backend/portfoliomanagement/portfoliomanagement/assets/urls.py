01e41291171dff2e2f8aa68e7db2d6b029399a55
00shashank08 <shashankdas14@gmail.com>
2024-03-30 18:52:04 +0530

    Summary of all current Investments in Natural Language

---
+++ backend/portfoliomanagement/portfoliomanagement/assets/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('assets', views.AssetListCreateView.as_view(), name='asset-list-create'),
+    path('assets/<str:pk>/',views. AssetRetrieveUpdateDestroyView.as_view(), name='asset-detail'),
+    path('assets/bulk_insert',views.insert_csv, name='bulk-insert')
+
+    # path('add',views.insert_asset),
+    # path('edit/<str:pk>', views.edit_asset, name='edit-asset'),
+    # path('remove/<str:pk>', views.remove_asset, name='remove-asset'),
+]
